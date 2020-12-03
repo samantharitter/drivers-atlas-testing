a9974e268ada0c1af8879cd83cc7601350cb9807
samantharitter <samantha.ritter@10gen.com>
2020-12-03 10:42:23 -0500

    workload signal handling working

--- integrations/c/workload-executor.c
+++ integrations/c/workload-executor.c
@@ -17,28 +17,29 @@
 #include <mongoc/mongoc.h>
 #include <stdio.h>
 
-typedef struct {
-    bool signaled;
-    bson_mutex_t mutex;
-} _signal_control_t;
+/* typedef struct { */
+/*     bool signaled; */
+/*     bson_mutex_t mutex; */
+/* } signal_control_t; */
 
-static _signal_control_t global_control;
+/* static signal_control_t *global_control; */
+
+volatile sig_atomic_t stop;
 
 void
 astrolabe_signal(int signal)
 {
     printf ("caught signal %d\n", signal);
-    // TODO set a flag of some sort.
+    stop = 1;
 }
 
 bson_t*
 parse_json (const char *json_blob)
 {
     bson_t *json;
-    long length;
     bson_error_t error;
 
-    json = bson_new_from_json ((const uint8_t *) json_blob, length, &error);
+    json = bson_new_from_json ((const uint8_t *) json_blob, -1, &error);
     if (!json) {
 	printf ("Error parsing json: %s\n", error.message);
     }
@@ -49,13 +50,7 @@ parse_json (const char *json_blob)
 bool
 signaled()
 {
-    bool signal;
-
-    bson_mutex_lock (global_control.mutex);
-    signal = global_control.signaled;
-    bson_mutex_unlock (global_control.mutex);
-
-    return signal;
+    return (stop == 1);
 }
 
 typedef struct {
@@ -67,21 +62,21 @@ typedef struct {
 void
 write_output (test_data_t *results)
 {
-    File *f;
+    FILE *f;
     bson_t bson;
     char *str;
 
     bson_init (&bson);
-    bson_append_int32 (&bson, "numErrors", -1, results.num_errors);
-    bson_append_int32 (&bson, "numFailures", -1, results.num_failures);
-    bson_append_int32 (&bson, "numSuccesses", -1, results.num_successes);
+    bson_append_int32 (&bson, "numErrors", -1, results->num_errors);
+    bson_append_int32 (&bson, "numFailures", -1, results->num_failures);
+    bson_append_int32 (&bson, "numSuccesses", -1, results->num_successes);
 
     str = bson_as_json (&bson, NULL);
 
     // Write to a file named results.json
-    f = fopen ("results.json", "rw");
+    f = fopen ("results.json", "w");
     if (!f) {
-	printf ("Error: could not open file to write results.\n");
+	printf ("Error: could not open file to write results, errno: %d\n", errno);
     } else {
         fputs (str, f);
         fclose (f);
@@ -91,27 +86,37 @@ write_output (test_data_t *results)
     bson_destroy (&bson);
 }
 
+bool
+run_operation ()
+{
+    sleep(1);
+    return true;
+}
+
 void
 run_tests (mongoc_client_t *client, bson_t *workload)
 {
     test_data_t results;
     bson_iter_t iter;
-    bool signaled;
     bool res;
 
+    // TODO: parse operations out and then actually run them
+    
     while (true) {
-	/* Check signal flag */
-	if (signaled()) {
-	    write_output (&results);
-	    return;
-	}
+	for (int i = 0; i < 6; i++) {
+	    printf ("loop %d\n", i);
+	    /* Check signal flag */
+	    if (signaled()) {
+		write_output (&results);
+		return;
+	    }
 
-	for (operation : workload) {
-	    bool res = run_opreation (db, coll, opration);
+	    bool res = run_operation();
 	    if (res) {
 		results.num_successes++;
 	    } else {
 		// determine if it was an error or a failure.
+		results.num_failures++;
 	    }
 	}
     }
@@ -125,6 +130,7 @@ main (int argc, char *argv[])
     const char *uri_str;
     const char *json_blob;
     bson_error_t error;
+    bson_t *workload;
 
     /* Install signal handler. */
 #if defined(_WIN64) || defined(_WIN32)
@@ -151,11 +157,23 @@ main (int argc, char *argv[])
 	return EXIT_FAILURE;
     }
 
+    /* Parse JSON input */
+    printf ("json blob is %s\n", json_blob);
+    workload = parse_json (json_blob);
+    if (!workload) {
+	mongoc_uri_destroy (uri);
+	return EXIT_FAILURE;
+    }
+
     client = mongoc_client_new_from_uri (uri);
+
+    //global_control = (signal_control_t *) bson_malloc0 (sizeof (struct signal_control_t));
+    run_tests (client, workload);
 
     /* Clean up before exiting. */
     mongoc_client_destroy (client);
     mongoc_uri_destroy (uri);
+    //bson_free (global_control);
 
     return EXIT_SUCCESS;
 }
